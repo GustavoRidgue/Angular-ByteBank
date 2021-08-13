3ad9489f5dcdb8ea7d16cb2f5ad91a0dca151948
gustavo.barbosa <gustavo.barbosa@omni.com.br>
2021-08-13 10:13:40 -0300

    Upgrade layout and get data from inputs

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -6,5 +7,22 @@ import { Component } from "@angular/core";
     styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent {
+    @Output() aoTransferir = new EventEmitter();
 
+    valor: number = 0.00;
+    destino: string = '0000-0';
+
+    transferir() {
+        console.log('Solicitado transferencia');
+
+        const valorEmitir = {
+            valor: this.valor,
+            destino: this.destino
+        };
+
+        this.aoTransferir.emit(valorEmitir);
+
+        // console.log(this.valor);
+        // console.log(this.destino);
+    }
 }
